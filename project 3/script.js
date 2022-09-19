d340d064090c13a2dc800802d6adc60fc59bd1cc
Armash khan <79808235+armash-khan97@users.noreply.github.com>
2022-09-20 03:24:15 +0500

    Project 3 HTML & CSS
    Completed HTML & CSS for Project 3

---
+++ project 3/script.js
@@ -0,0 +1,8 @@
+// Get DOM Elemnt
+const video = document.getElementById('video');
+const play = document.getElementById('paly');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
+
+// 
